19e44ed114cbb6b41647b98f6a6e617dd8ddadd2
Ruizerick26 <ruizerick2525@gmail.com>
2023-12-10 18:17:20 -0500

    Segunda parte

---
+++ src/AREAS/Cuadrado.java
@@ -0,0 +1,15 @@
+package AREAS;
+
+public class Cuadrado {
+	int lado;
+	public double area() {
+		double resultado;
+		resultado = lado *4;
+		return resultado;
+	}
+	public double perimeroC() {
+		double resultado;
+		resultado = lado * 4;
+		return resultado;
+	}
+}
---
+++ src/AREAS/Testcuadrado.java
@@ -0,0 +1,21 @@
+package AREAS;
+
+public class Testcuadrado {
+	public static void main(String[] args) {
+		Cuadrado c1 = new Cuadrado();
+		Cuadrado c2 = new Cuadrado();
+		Cuadrado c3 = new Cuadrado();
+		c1.lado=4;
+		c2.lado=5;
+		c3.lado=6;
+		double resultadoc11 = c1.area();
+		double resultadoc12 = c1.perimeroC();
+		double resultadoc21 = c2.area();
+		double resultadoc22 = c2.perimeroC();
+		double resultadoc31 = c3.area();
+		double resultadoc32 = c3.perimeroC();
+		System.out.println("El área y perimetro del C1 es: " +resultadoc11 + " y " + resultadoc12);
+		System.out.println("El área y perimetro del C2 es: " +resultadoc21 + " y " + resultadoc22);
+		System.out.println("El área y perimetro del C3 es: " +resultadoc31 + " y " + resultadoc32);
+	}
+}
